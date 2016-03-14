4a93b1d5c9739c9ce1ddb68a84197a08a5da2881
Star_Grif <grifon.km@gmail.com>
2016-03-15 01:01:49 +0300

    Check avaliable sensor. Add libs for its

--- Actuator/Actuator_FW/Actuator_FW/Actuator_FW.ino
+++ Actuator/Actuator_FW/Actuator_FW/Actuator_FW.ino
@@ -5,6 +5,11 @@
 */
 
 // the setup function runs once when you press reset or power the board
+#include <MPU6050_9Axis_MotionApps41.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <MPU6050.h>
+#include <helper_3dmath.h>
+
 void setup() {
 
 }
