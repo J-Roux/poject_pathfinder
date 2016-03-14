1682a88fa8088337d77ffd379cd8c59343177295
StarGrif <grifon.km@gmail.com>
2016-03-14 17:45:10 +0300

    Init branch. Add free project

---
+++ Actuator/Actuator_FW/Actuator_FW/Actuator_FW.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Actuator_FW.ino
+ Created:	14.03.2016 17:41:14
+ Author:	max
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
