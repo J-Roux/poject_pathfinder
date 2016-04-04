5485913df13a19e4193a7f658a35c7c5517e6aee
StarGrif <grifon.km@gmail.com>
2016-04-04 22:25:26 +0300

    Fix mpu data output.

--- Actuator/Actuator_FW/Actuator_FW/Actuator_FW.ino
+++ Actuator/Actuator_FW/Actuator_FW/Actuator_FW.ino
@@ -2,37 +2,90 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
 #include "I2Cdev.h"
-#include "MPU6050.h"
-
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
+#include "MPU6050_6Axis_MotionApps20.h"
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
-MPU6050 accelgyro;
+MPU6050 mpu;
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define OUTPUT_READABLE_REALACCEL
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+						// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+						// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+	mpuInterrupt = true;
+}
 
 void setup(void)
 {
-	Serial.begin(38400);
+	Serial.begin(115200);
+	while (!Serial);
+
 	Wire.begin();
 
 	Serial.println("Initializing I2C devices...");
 
-	accelgyro.initialize();
-	if (!accelgyro.testConnection())
-	{
-		Serial.println("MPU6050 connection failed");
-		while (1);
-	}	
+	mpu.initialize();
+	Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+	devStatus = mpu.dmpInitialize();
 
-	accelgyro.setXGyroOffset(220);
-	accelgyro.setYGyroOffset(76);
-	accelgyro.setZGyroOffset(-85);
-	accelgyro.setZAccelOffset(1788); // 1688 factory default for my test chip
+	// supply your own gyro offsets here, scaled for min sensitivity
+	mpu.setXGyroOffset(220);
+	mpu.setYGyroOffset(76);
+	mpu.setZGyroOffset(-85);
+	mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+							   // make sure it worked (returns 0 if so)
+	if (devStatus == 0) {
+		// turn on the DMP, now that it's ready
+		Serial.println(F("Enabling DMP..."));
+		mpu.setDMPEnabled(true);
+
+		// enable Arduino interrupt detection
+		Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+		attachInterrupt(0, dmpDataReady, RISING);
+		mpuIntStatus = mpu.getIntStatus();
+
+		// set our DMP Ready flag so the main loop() function knows it's okay to use it
+		Serial.println(F("DMP ready! Waiting for first interrupt..."));
+		dmpReady = true;
+
+		// get expected DMP packet size for later comparison
+		packetSize = mpu.dmpGetFIFOPacketSize();
+	}
+	else {
+		// ERROR!
+		// 1 = initial memory load failed
+		// 2 = DMP configuration updates failed
+		// (if it's going to break, usually the code will be 1)
+		Serial.print(F("DMP Initialization failed (code "));
+		Serial.print(devStatus);
+		Serial.println(F(")"));
+		while (true);
+	}
 
 	if (!mag.begin())
 	{
@@ -45,53 +98,79 @@ void setup(void)
 
 void loop(void)
 {
-	/* Get a new sensor event */
+	// if programming failed, don't try to do anything
+	if (!dmpReady) return;
+
+	// wait for MPU interrupt or extra packet(s) available
+	while (!mpuInterrupt && fifoCount < packetSize);
+
+	// reset interrupt flag and get INT_STATUS byte
+	mpuInterrupt = false;
+	mpuIntStatus = mpu.getIntStatus();
+
+	// get current FIFO count
+	fifoCount = mpu.getFIFOCount();
+
+	// check for overflow (this should never happen unless our code is too inefficient)
+	if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+		// reset so we can continue cleanly
+		mpu.resetFIFO();
+		Serial.println(F("FIFO overflow!"));
+
+		// otherwise, check for DMP data ready interrupt (this should happen frequently)
+	}
+	else if (mpuIntStatus & 0x02) {
+		// wait for correct available data length, should be a VERY short wait
+		while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+		// read a packet from FIFO
+		mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+		// track FIFO count here in case there is > 1 packet available
+		// (this lets us immediately read more without waiting for an interrupt)
+		fifoCount -= packetSize;
+
+
+		// display Euler angles in degrees
+		mpu.dmpGetQuaternion(&q, fifoBuffer);
+		mpu.dmpGetGravity(&gravity, &q);
+		mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+		Serial.print("ypr\t");
+		Serial.print(ypr[0] * 180 / M_PI);
+		Serial.print("\t");
+		Serial.print(ypr[1] * 180 / M_PI);
+		Serial.print("\t");
+		Serial.println(ypr[2] * 180 / M_PI);
+
+		// display real acceleration, adjusted to remove gravity
+		mpu.dmpGetQuaternion(&q, fifoBuffer);
+		mpu.dmpGetAccel(&aa, fifoBuffer);
+		mpu.dmpGetGravity(&gravity, &q);
+		mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+		Serial.print("areal\t");
+		Serial.print(aaReal.x);
+		Serial.print("\t");
+		Serial.print(aaReal.y);
+		Serial.print("\t");
+		Serial.println(aaReal.z);
+
+	}
+
+	// magnitometer
 	sensors_event_t event;
 	mag.getEvent(&event);
 
 	float heading = atan2(event.magnetic.y, event.magnetic.x);
-	//float pitch = atan2(event.magnetic.y, event.magnetic.z);
-
-
-	// Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
-	// Find yours here: http://www.magnetic-declination.com/
-	// Mine is: -13* 2' W, which is ~13 Degrees, or (which we need) 0.22 radians
-	// If you cannot find your Declination, comment out these two lines, your compass will be slightly off.
 	float declinationAngle = 0.1338;
 	heading += declinationAngle;
-	//pitch += declinationAngle;
 
-	// Correct for when signs are reversed.
 	if (heading < 0)
 		heading += 2 * PI;
 
-	// Check for wrap due to addition of declination.
 	if (heading > 2 * PI)
 		heading -= 2 * PI;
 
-	// Convert radians to degrees for readability.
 	float headingDegrees = heading * 180 / M_PI;
-	//float pitchDegrees = pitch * 180 / M_PI;
-
 	Serial.print("Heating: "); 
-	Serial.println(headingDegrees);
-	//Serial.print(" ");
-	//Serial.println(pitchDegrees);
-
-	accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
-	/*Serial.print("a/g:\t");
-	Serial.print(ax); Serial.print("\t");
-	Serial.print(ay); Serial.print("\t");
-	Serial.print(az); Serial.print("\t");
-	Serial.print("/");
-	Serial.print(gx); Serial.print("\t");
-	Serial.print(gy); Serial.print("\t");
-	Serial.println(gz);*/
-
-	float pitchDegrees = atan2(gy, gz) * 180 / M_PI;
-	Serial.print("Pitch: "); 
-	Serial.println(pitchDegrees);
-		
-	delay(1000);
+	Serial.println(headingDegrees);	
 }
